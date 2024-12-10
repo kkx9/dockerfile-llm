6e1bae52a9ebf34ed05d0f8a2e2ea2964f3ba062
kkx9 <64183990+kkx9@users.noreply.github.com>
2024-12-10 10:26:38 +0800

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -3,6 +3,7 @@
 # .\msedge.exe --remote-debugging-port=9222 --user-data-dir="D:\AutomationProfile\edge"
 
 import json
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -17,11 +18,12 @@ try:
     # topic = "dockerfile"
     pages = 100
     pagesize = 50
-    for page in range(30, pages+1):
+    position = 8
+    for page in range(1, pages+1):
         for index in range(1, pagesize+1):
             driver.get(f"https://stackoverflow.com/questions/tagged/{topic}?page={page}&sort=votes&pagesize={pagesize}")
             time.sleep(1)
-            question_link = driver.find_element(By.XPATH, f"/html/body/div[8]/div[3]/div[1]/div[3]/div[{index}]/div[2]/h3/a")
+            question_link = driver.find_element(By.XPATH, f"/html/body/div[{position}]/div[3]/div[1]/div[3]/div[{index}]/div[2]/h3/a")
             question_url = question_link.get_attribute("href")
             driver.get(question_url)
             time.sleep(1)
@@ -30,11 +32,24 @@ try:
             print(f"Question Title: {question_title}")
 
             try:
-                answer = driver.find_element(By.XPATH, "/html/body/div[8]/div[3]/div/div[1]/div[3]/div[3]/div[3]/div/div[2]/div[1]").text
+                try:
+                    try:
+                        try:
+                            answer = driver.find_element(By.XPATH, f"/html/body/div[{position}]/div[3]/div/div[1]/div[3]/div[3]/div[3]/div/div[2]/div[1]").text
+                        except Exception as e:
+                            answer = driver.find_element(By.XPATH, f"/html/body/div[{position}]/div[3]/div/div[1]/div[3]/div[3]/div[2]/div/div[2]/div[1]").text
+                    except Exception as e:
+                        answer = driver.find_element(By.XPATH, f"/html/body/div[{position}]/div[3]/div/div[1]/div[3]/div[3]/div[3]/div/div[2]/div[2]").text
+                except Exception as e:
+                    answer = driver.find_element(By.XPATH, f"/html/body/div[{position}]/div[3]/div/div[1]/div[3]/div[3]/div[2]/div/div[2]/div[2]").text
             except Exception as e:
-                answer = driver.find_element(By.XPATH, "/html/body/div[8]/div[3]/div/div[1]/div[3]/div[3]/div[2]/div/div[2]/div[1]").text
+                continue
+            if len(answer) < 1:
+                continue
             print(f"Question Answer: {answer}")
 
+            if not os.path.exists("dockerfile.json"):
+                open("dockerfile.json", "w").close()
             with open(f"{topic}.json", "r", encoding="utf-8") as result:
                 try:
                     data = json.load(result)
